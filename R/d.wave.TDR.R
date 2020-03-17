fb35f80bdf287d40ae24d01a6cf780033022ddac
Martin Biuw <martin.biuw@hi.no>
2020-03-17 16:55:09 +0100

    Added original version of d.wave.TDR without parameter settings option

--- R/d.wave.TDR.R
+++ R/d.wave.TDR.R
@@ -56,6 +56,60 @@ d.wave.TDR <- function(tdr.file='./Data/HVTag/Rfiles/Whale_2015Feb23.RData',
   }
 }
 
+
+#' \code{d.wave.TDR.original} 2D lunge detector for individual dive in a TDR record. Original backup version
+#' @param tdr.file R object containing pre-processed dive record.
+#' If set to NA, this function can run the detector on data from all individuals
+#' with data records stored in the directory specified in \code{tdr.folder}.
+#' @param tdr.folder Folder in which TDR files are stored.
+#' @details Run the 2D lunged detector on TDR-type dive data, i.e. this version has no validation against pre-determined lunges
+#' @family Lunge detector functions
+#' @seealso \code{\link{run.d.wave}} for data type with validation against pre-determined lunges.
+#' @author Martin Biuw
+#' @examples
+#' tdr.list <- d.wave.TDR.original(tdr.file=NA)
+#' @export
+
+d.wave.TDR.original <- function(tdr.file='./Data/HVTag/Rfiles/Whale_2015Feb23.RData',
+                       tdr.folder='./Data/HVTag/Rfiles') {
+  if(!is.na(tdr.file)) {
+    load(tdr.file)
+    whalename <- tail(unlist(strsplit(tdr.file, '/')),1)
+    whalename <- gsub('.RData', '', whalename)
+    eval(parse(text=paste('tdr <- ', whalename, '$original')))
+    cat('Running 2D lunge detector for whale', whalename, '     \n')
+    flush.console()
+    tdr <- run.d.wave(data=tdr)
+    tdr
+  } else {
+    tdr.file <- paste(tdr.folder, dir(tdr.folder), sep='/')
+    tdr.list <- vector('list', length(dir(tdr.folder)))
+    names(tdr.list) <- gsub('.RData', '', dir(tdr.folder))
+    ## Note! Names ended up inconsistent from w=32 onwards.
+    ## Fixed it externally, but check what's wrong here.
+
+    for(w in 1:length(tdr.file)) {
+      load(tdr.file[w])
+      whalename <- tail(unlist(strsplit(tdr.file[w], '/')),1)
+      whalename <- gsub('.RData', '', whalename)
+      eval(parse(text=paste('tdr <- ', whalename)))
+      cat('Running 2D lunge detector for whale',
+          whalename, '(file', w, 'of', length(tdr.file), ')     \r')
+      flush.console()
+      tdr <- try(run.d.wave(data=tdr$original), silent=T)
+      if(class(tdr)!='try-error') {
+        ##        tdr.tmp$original <- tdr$df
+        ##        dg <- mkDygraph(tdr)
+        ##        tdr.tmp$graph <- dg
+        ##        tdr.list[[w]] <- tdr.tmp
+        tdr.list[[w]] <- tdr
+        ##eval(parse(text=paste(whalename, '<- tdr.tmp')))
+      }
+    }
+    tdr.list
+  }
+}
+
 #' \code{filter.lunges} Filter out lunges during short & shallow dives
 #' @param data Output from running \code{d.wave.TDR}.
 #' @param duration Maximum dive duration of dives considered for filtering out lunges
